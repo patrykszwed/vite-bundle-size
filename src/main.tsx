9f0e2caddf6b36eb09fa0eaf934b5210ec62a7bd
patryk.szwed <patryk.szwed@outlook.com>
2024-04-16 14:19:09 +0200

    init

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import App from './App.tsx'
+import './index.css'
+import { render } from 'react-dom';
+
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+    document.getElementById('root')
+)
